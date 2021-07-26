f02202efcb2b7b3f8a274c0cd9a0459a5f7e5d5b
zchalk <you@example.com>
2021-07-26 00:32:50 -0400

    added basic backend

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas/index');
+const { AuthMiddleware } = require('./utils/auth');
+
+const db = require('./config/config');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: AuthMiddleware
+});
+
+server.applyMiddleware({app});
+
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({ limit: '50mb', parameterLimit: 50000, extended: true}));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(publicPath, 'index.html'))
+    });
+}
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+});
